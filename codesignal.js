8daf2792a1420737ec62c2ea6e7d6688cff5ca15
Monica Zarate <monicazaratecortes@gmail.com>
2021-03-10 18:53:37 -0800

    Problem of the day.

--- codesignal.js
+++ codesignal.js
@@ -1,9 +1,23 @@
 //Write a function that returns the sum of two numbers.
 
-function add(param1, param2) {
-  let result = param1 + param2;
+// function add(param1, param2) {
+//   let result = param1 + param2;
 
-  return result;
+//   return result;
+// }
+
+// add(10, 8);
+
+//Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+function centuryFromYear(year) {
+  let century = 0;
+  if (year % 100 === 0) {
+    century = Math.floor(year / 100);
+  } else {
+    century = Math.floor(year / 100) + 1;
+  }
+  return century;
 }
 
-add(10, 8);
+//centuryFromYear(1905);
