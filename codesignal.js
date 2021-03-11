b2970da93bcd980d66c44d9a79d518f6650fd7ab
Monica Zarate <monicazaratecortes@gmail.com>
2021-03-11 12:25:05 -0800

    Problem of the day.

--- codesignal.js
+++ codesignal.js
@@ -10,14 +10,33 @@
 
 //Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
 
-function centuryFromYear(year) {
-  let century = 0;
-  if (year % 100 === 0) {
-    century = Math.floor(year / 100);
-  } else {
-    century = Math.floor(year / 100) + 1;
-  }
-  return century;
-}
+// function centuryFromYear(year) {
+//   let century = 0;
+//   if (year % 100 === 0) {
+//     century = Math.floor(year / 100);
+//   } else {
+//     century = Math.floor(year / 100) + 1;
+//   }
+//   return century;
+// }
 
 //centuryFromYear(1905);
+
+//Given the string, check if it is a palindrome.
+function checkPalindrome(inputString) {
+  let chars = "";
+  let isPalindrome;
+  for (i = inputString.length - 1; i >= 0; i--) {
+    chars = chars + inputString[i];
+  }
+
+  if (chars === inputString) {
+    isPalindrome = true;
+  } else {
+    isPalindrome = false;
+  }
+
+  return isPalindrome;
+}
+
+checkPalindrome("aabaa");
